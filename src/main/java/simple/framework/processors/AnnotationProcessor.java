d9ce6d6fa1ad1a83608b3717f454dcae0870ee90
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-08-05 16:46:13 +0300

    Class loading

--- src/main/java/simple/framework/processors/AnnotationProcessor.java
+++ src/main/java/simple/framework/processors/AnnotationProcessor.java
@@ -1,33 +1,57 @@
 package simple.framework.processors;
 
+import simple.framework.annotations.Init;
 import simple.framework.annotations.Service;
 import simple.framework.services.LazyService;
 import simple.framework.services.SimpleService;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class AnnotationProcessor {
+    static Map<String, Object> classes = new HashMap<>();
+
     public static void main(String[] args) {
         inspectService(SimpleService.class);
         inspectService(LazyService.class);
-        inspectService(String.class);
-        /*
-        Super simple service
-        false
-        Very-very lazy service
-        true
-         */
+
+        loadClass("simple.framework.services.SimpleService");
+        loadClass("simple.framework.services.LazyService");
+        loadClass("java.lang.String");
+    }
+
+    private static void loadClass(String name) {
+        try {
+            Class<?> aClass = Class.forName(name);
+            if(aClass.isAnnotationPresent(Service.class)){
+                Object object = aClass.newInstance();
+                classes.put(name, object);
+                System.out.println("Class successfully created: " + name);
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Class not found: " + name);
+        } catch (IllegalAccessException | InstantiationException e) {
+            System.err.println("Class can't be created: " + name);
+        }
     }
 
     static void inspectService(Class<?> service) {
-//        boolean isPresent = service.isAnnotationPresent(Init.class);
-//        Init annotation = service.getAnnotation(Init.class);
-//        Annotation[] annotations = service.getAnnotations();
-//        Annotation[] declaredAnnotations = service.getDeclaredAnnotations();
 
         if(service.isAnnotationPresent(Service.class)) {
             Service ann = service.getAnnotation(Service.class);
             System.out.println(ann.name());
             System.out.println(ann.lazyLoad());
         }
+        for (Method method : service.getDeclaredMethods()) {
+            System.out.print(method.getName() + " : ");
+            if(method.isAnnotationPresent(Init.class)){
+                System.out.println("found");
+            } else {
+                System.out.println("not found");
+            }
+        }
+
     }
 }
